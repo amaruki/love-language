aeff8b1d90eae49808a1fd9f6447dd33c9a9862b
Ngapa <krilinamar@gmail.com>
2025-03-28 17:13:13 +0700

    Erase Firebase Analytics

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -17,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
+
